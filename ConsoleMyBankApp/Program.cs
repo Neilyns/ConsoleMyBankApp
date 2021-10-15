306724a5412524fd506252c4c0fadb6c100e5229
Neilyane Silva <neilyane.ssouza1@gmail.com>
2021-10-14 22:33:03 -0300

    Atualização classes 2

--- ConsoleMyBankApp/Program.cs
+++ ConsoleMyBankApp/Program.cs
@@ -6,7 +6,19 @@ namespace ConsoleMyBankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cliente cliente = new Cliente("Neilyane Silva", "000.000.000-00", "Analista de Planejamento");
+
+           
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = cliente;
+            conta.agencia = 01;
+            conta.conta = 0001;
+
+            Console.Write("Conta Corrente Criada ");
+            Console.WriteLine("Titular: {0}", conta.titular.nome);
+            Console.WriteLine("Agência: {0}", conta.agencia);
+            Console.WriteLine("Conta: {0}", conta.conta);
         }
     }
 }
